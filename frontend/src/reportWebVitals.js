36e2497417757fcd38cb0271c6e6e45845af9b01
Nexgen-Agent <vukonamongwe963@gmail.com>
2025-07-01 10:54:56 +0200

    Update reportWebVitals.js

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,3 +1,7 @@
+// reportWebVitals.js
+// This file enables performance measuring for your luxury web app.
+// Learn more: https://bit.ly/CRA-vitals
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
